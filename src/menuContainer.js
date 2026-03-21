422163cfed2b1dfdcc503828af88c7d58d8225fb
Hanabata <93629352+hanabata84@users.noreply.github.com>
2026-03-22 07:56:18 +0900

    added a menu container

---
+++ src/menuContainer.js
@@ -0,0 +1,36 @@
+import { pizza } from "./pizzaMenu.js";
+
+const menuDiv = document.createElement('div');
+menuDiv.setAttribute('id', 'menu-container');
+
+pizza.forEach(item => {
+    const itemDiv = document.createElement('div');
+    itemDiv.setAttribute('class', 'item-cont');
+    
+    const img = document.createElement('img');
+    img.src = item.img;
+
+    const pName = document.createElement('p');
+    pName.textContent = item.name;
+
+    const pDesc = document.createElement('p');
+    if(item.description.length > 100) {
+        pDesc.textContent = `${item.description.substring(0, 100)}...read more`
+    } else {
+        pDesc.textContent = item.description;
+    }
+    
+
+    const pPrice = document.createElement('p');
+    pPrice.textContent = item.price;
+
+    itemDiv.appendChild(img)
+    itemDiv.appendChild(pName)
+    itemDiv.appendChild(pDesc)
+    itemDiv.appendChild(pPrice)
+
+    menuDiv.appendChild(itemDiv)
+});
+
+
+export { menuDiv }
--- src/menuPage.js
+++ src/menuPage.js
@@ -1,3 +1,5 @@
+import { menuDiv } from "./menuContainer.js";
+
 const menuOverlay = document.createElement('div');
 menuOverlay.setAttribute('class', 'overlay')
 
@@ -7,6 +9,8 @@ h2.textContent = 'Menu';
 const p = document.createElement('p');
 p.textContent = 'pizza | toppings | cheese'
 
-menuOverlay.appendChild(h2)
-menuOverlay.appendChild(p)
+menuOverlay.appendChild(h2);
+menuOverlay.appendChild(p);
+menuOverlay.appendChild(menuDiv);
+
 export {menuOverlay}
