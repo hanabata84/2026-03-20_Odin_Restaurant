891d783077f52b9e379ec58aa8eb8ac2f47a3778
Hanabata <93629352+hanabata84@users.noreply.github.com>
2026-03-21 19:21:57 +0900

    button clear contents for menu and about

---
+++ src/aboutPage.js
@@ -0,0 +1,4 @@
+const p2 = document.createElement('p');
+p2.textContent = 'About';
+
+export {p2}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import "./style.css"
 import { heroDiv } from "./homePage.js"
 import { p1 } from "./menuPage.js";
+import { p2 } from "./aboutPage.js";
 
 const content = document.querySelector('#content');
 content.appendChild(heroDiv);
 
 const menuBtn = document.querySelector('#menu-btn');
-menuBtn.addEventListener('click', ()=>content.appendChild(p1))
+menuBtn.addEventListener('click', ()=>{
+    content.replaceChildren();
+    content.appendChild(p1);
+})
+
+const aboutBtn = document.querySelector('#about-btn');
+aboutBtn.addEventListener('click', ()=>{
+    content.replaceChildren();
+    content.appendChild(p2)
+})
