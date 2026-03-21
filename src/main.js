8c0c552eeb7f9b8997f8edc3a35635f460469d0f
Hanabata <93629352+hanabata84@users.noreply.github.com>
2026-03-21 19:28:12 +0900

    added eventlistener to home button

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import { p2 } from "./aboutPage.js";
 const content = document.querySelector('#content');
 content.appendChild(heroDiv);
 
+const homeBtn = document.querySelector('#home-btn');
+homeBtn.addEventListener('click', ()=> {
+    content.replaceChildren();
+    content.appendChild(heroDiv);
+})
+
 const menuBtn = document.querySelector('#menu-btn');
 menuBtn.addEventListener('click', ()=>{
     content.replaceChildren();
