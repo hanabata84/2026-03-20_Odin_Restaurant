4217babc5adedfcdcfb214207109c63531734581
Hanabata <93629352+hanabata84@users.noreply.github.com>
2026-03-22 07:16:04 +0900

    added pizza menu object

---
+++ src/pizzaMenu.js
@@ -0,0 +1,23 @@
+const pizza = [
+    {
+        id: 1,
+        name: "peperroncini",
+        description: "Pizza topped with pepperoncini peppers, which are small, mild, tangy, and slightly spicy pickled peppers originally from Italy and Greece.  These peppers add a briny, zesty flavor that balances rich ingredients like mozzarella, garlic, onions, salami, or olives.",
+        img: "./assets/pepperoncini.jpg",
+        price: 1500    
+    },
+    {
+        id: 2,
+        name: "margeritha",
+        description: "a classic Italian dish, more accurately known as Margherita pizza, originating in Naples.  It is traditionally made with a thin crust topped with crushed tomatoes (often San Marzano), fresh mozzarella cheese, fresh basil leaves, and a drizzle of extra virgin olive oil.",
+        img: "./assets/margarita.jpg",
+        price: 1200    
+    },
+    {
+        id: 3,
+        name: "quatro cheese",
+        description: "a classic Italian pizza style meaning four cheeses in Italian.  It features a combination of four distinct cheeses, typically mozzarella, gorgonzola, fontina, and Parmigiano Reggiano, creating a rich, balanced, and creamy flavor profile.  The mozzarella acts as a base, maintaining humidity during baking to protect the other cheeses from excessive heat. Gorgonzola adds a bold, tangy depth, while fontina brings creaminess and Parmigiano Reggiano contributes a nutty, salty finish.",
+        img: "./assets/quatro-cheese.jpg",
+        price: 1200    
+    }
+]
